a69a5485f0d96060f8710ef59cff9cb8bed40d3b
virajnilakh <virajnilakh220@gmail.com>
2017-04-23 14:35:24 -0700

    Queuing

---
+++ src/gash/router/server/InboundCommandMessageQueueHandler.java
@@ -0,0 +1,13 @@
+package gash.router.server;
+
+public class InboundCommandMessageQueueHandler implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			
+		}
+	}
+
+}
---
+++ src/gash/router/server/InboundWorkMessageQueueHandler.java
@@ -0,0 +1,13 @@
+package gash.router.server;
+
+public class InboundWorkMessageQueueHandler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			
+		}
+	}
+
+}
